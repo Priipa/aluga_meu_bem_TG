011d1b67df0b8d5607509d201f8d99af6407df18
Giovanna Priscila Nascimento <pripa.diflen@gmail.com>
2026-09-08 17:48:23 -0300

    chore: adicionar estrutura inicial do Ionic Angular

---
+++ alugameubem/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.alugameubem.app',
+  appName: 'Aluga, meu bem!',
+  webDir: 'www'
+};
+
+export default config;
